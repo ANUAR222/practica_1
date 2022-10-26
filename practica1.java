38b901ac66004ba77a2323dbdf259228cecdcb87
Anuar <anuarmohame2@gmail.com>
2022-10-26 18:36:23 +0200

    svx

--- practica1.java
+++ practica1.java
@@ -1,5 +1,9 @@
+
+
 public class practica1 {
-    public static void main(String[] args) {
-        System.out.print("hola que ase");
-    }
+public static void main(String[] args) {
+System.out.println("Hola Mundo");
+
+}
+
 }
