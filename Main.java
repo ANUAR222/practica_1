56ce33bf3bd7f1d3f370dcee4fa2b14afb15e6a9
Anuar <anuarmohame2@gmail.com>
2022-11-09 18:16:20 +0100

    ykmgj

--- Main.java
+++ Main.java
@@ -1,39 +1,53 @@
-public class Main {
-    public static void main(String[] args) {
-        int[] contador = new int[5];
-        for (int i = 0; i < 100; ++i) {
-            contador[Dado.getInstance().quienEmpieza(4)]++;
-        }
-        for (int i = 0; i < 5; ++i) {
-            System.out.println("El jugador " + i + " ha salido " + contador[i] + " veces.");
-        }
-        //2
-        System.out.println("Modo debug activado");
-        Dado.getInstance().setDebug(true);
-        for (int i = 0; i < 5; ++i) {
-            System.out.println(Dado.getInstance().tirar());
-        }
-        System.out.println("Modo debug desactivado");
-        Dado.getInstance().setDebug(false);
-        for (int i = 0; i < 5; ++i) {
-            System.out.println(Dado.getInstance().tirar());
-        }
-        //3
-        System.out.println("El último resultado ha sido " + Dado.getInstance().getUltimoResultado());
-        System.out.println("¿Salgo de la cárcel? " + Dado.getInstance().salgoDeLaCarcel());
-        //4
-        System.out.println("El valor de la casilla de Salida es " + tablero.Casilla.SALIDA);
-        System.out.println("El valor de la casilla de Cárcel es " + tablero.Casilla.CARCEL);
-        System.out.println("El valor de la casilla de Impuesto es " + tablero.Casilla.IMPUESTO);
-        System.out.println("El valor de la casilla de Juez es " + tablero.Casilla.JUEZ);
-        System.out.println("El valor de la casilla de Sorpresa es " + tablero.Casilla.SORPRESA);
-        //5
-        MazoSorpresas mazo = new MazoSorpresas();
-        mazo.alMazo(new Sorpresa("¡Ganaste 1000 euros!", 1000, tablero.Sorpresa.PAGARCOBRAR));
-        mazo.alMazo(new Sorpresa("¡Vas a la cárcel!", 0, tablero.Sorpresa.IRACASILLA));
-        System.out.println(mazo.siguiente().toString());
-        mazo.inhabilitarCartaEspecial(mazo.siguiente());
-        mazo.habilitarCartaEspecial(mazo.siguiente());
 
-    }
-}
+        public class Main {
+            public static void main(String[] args) {
+                int[] contador = new int[5];
+                Dado Dado = new Dado();
+                for (int i = 0; i < 100; ++i) {
+                    contador[Dado.quienEmpieza(4)]++;
+                }
+                for (int i = 0; i < 5; ++i) {
+                    System.out.println("El valor " + i + " ha salido " + contador[i] + " veces.");
+                }
+                //2
+                System.out.println("Modo debug activado");
+                Dado.setDebug(true);
+                for (int i = 0; i < 5; ++i) {
+                    System.out.println(Dado.tirar());
+                }
+                System.out.println("Modo debug desactivado");
+                Dado.setDebug(false);
+                for (int i = 0; i < 5; ++i) {
+                    System.out.println(Dado.tirar());
+                }
+                //3
+                System.out.println("El último resultado ha sido " + Dado.getUltimoResultado());
+                System.out.println("¿Salgo de la cárcel? " + Dado.salgoDeLaCarcel());
+                //4
+                System.out.println("El valor de la casilla de Salida es " + Casilla.SALIDA);
+                System.out.println("El valor de la casilla de Cárcel es " + Casilla.CARCEL);
+                System.out.println("El valor de la casilla de Impuesto es " + Casilla.IMPUESTO);
+                System.out.println("El valor de la casilla de Juez es " + Casilla.JUEZ);
+                System.out.println("El valor de la casilla de Sorpresa es " + Casilla.SORPRESA);
+                System.out.println("El valor de la casilla de Descanso es " + Casilla.DESCANSO);
+                System.out.println("El valor de la casilla de Calle es " + Casilla.CALLE);
+                System.out.println("El valor de la casilla de Aparcmiento es " + Casilla.APARCAMIENTO);
+                //printea los enmumerados sorpresa
+                System.out.println("El valor de la sorpresa de ir a la cárcel es " + Sorpresa.IRCARCEL);
+                System.out.println("El valor de la sorpresa de ir a la casilla es " + Sorpresa.IRCASILLA);
+                System.out.println("El valor de la sorpresa de ir a la PAGARCOBRAR es " + Sorpresa.PAGARCOBRAR);
+                System.out.println("El valor de la sorpresa de ir a la POR CASA HOTEL es " + Sorpresa.PORCASAHOTEL);
+                System.out.println("El valor de la sorpresa de ir a la POR JUGADOR es " + Sorpresa.PORJUGADOR);
+                System.out.println("El valor de la sorpresa de ir a la SALIR CARCEL es " + Sorpresa.SALIRCARCEL);
+
+
+                /*6. usa la clase Diario, aprovecha y prueba todos los métodos de Diario*/
+                civitas.Diario diario = civitas.Diario.getInstance();
+                diario.getInstance().ocurreEvento("Evento 1");
+                diario.getInstance().ocurreEvento("Evento 2");
+                diario.getInstance().ocurreEvento("Evento 3");
+
+
+
+            }
+        }
