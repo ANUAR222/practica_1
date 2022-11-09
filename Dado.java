b9d112ac05b46b73bcfd259a674a680a314b1162
Piloto2003 <46202382+Piloto2003@users.noreply.github.com>
2022-11-09 17:16:22 +0100

    Add files via upload

--- Dado.java
+++ Dado.java
@@ -1,21 +1,44 @@
 import java.util.Random;
 
+
 public class Dado {
-        int SalidaCarcel = 5;
-    public static void main(String [] args){
-        int inmovil;
-        inmovil = 0;
-        Random Numerodado = new Random();
-        int ultimoResultado = 1 + Numerodado.nextInt(6);
-        boolean permisodado;
-        if (inmovil>0){
-            permisodado = false;
-            inmovil -= 1;
-        } else {
-            permisodado = true;
+    private Random Numerodado = new Random();
+    private int ultimoResultado = 1 + Numerodado.nextInt(6);
+    private boolean debug;
+    private static int SalidaCarcel = 5;
+
+    public int tirar(){
+       if (debug == true) {
+           ultimoResultado = 1;
+       }
+           return 0;
+    }
+
+    public boolean salgoDeLaCarcel() {
+        if(debug == true){
+            boolean salgoDeLaCarcel = true;
         }
-        if(permisodado) {
-            System.out.println(ultimoResultado);
+        return false;
+    }
+    public int quienEmpieza(int n){
+        n=0;
+        for (int e=1;e>=4;n--){
+            System.out.println("El ");
+            System.out.println(e);
+            System.out.println("º en salir es ");
+            System.out.println(n);
         }
+        return -1;
+    }
+
+    public void setDebug (Boolean d){
+        debug=d;
+        if(debug==true) {
+            civitas.Diario.ocurreEvento("debug");
+        }
+
+}
+    public int getUltimoResultado(){
+    return ultimoResultado;
     }
 }
