a5320a81d5507c94a2fef162351003e6998a0f44
SirTimplen <116745740+SirTimplen@users.noreply.github.com>
2022-12-02 20:09:22 +0100

    Update Jugador.java

--- Jugador.java
+++ Jugador.java
@@ -30,66 +30,6 @@ public class Jugador {
         this.puedeComprar=otro.puedeComprar;
         this.salvoconducto=otro.salvoconducto;
     }
-    /*Boolean debeSerEncarcelado (): este método siempre devuelve false si el jugador ya está
-encarcelado. En caso contrario, si no tiene la carta sorpresa que permite evitar la cárcel
-(tieneSalvoconducto()) devolverá true. En caso de que disponga de dicha carta para librarse
-de la cárcel, la pierde por usarla (perderSalvoconducto()), se informa al diario de que el
-jugador se libra de la cárcel y se devuelve false.
-• Boolean encarcelar (int numCasillaCarcel): si el jugador debe ser encarcelado
-(debeSerEncarcelado ()), mueve (moverACasilla) al jugador a la casilla indicada como
-parámetro (que debería ser el índice de la cárcel), cambia el valor del atributo encarcelado a
-true e informa al diario de lo ocurrido. Siempre devuelve el valor del citado atributo
-encarcelado.
-• Boolean obtenerSalvoconducto (Sorpresa s): si el jugador está encarcelado no se realiza
-ninguna acción salvo devolver false. En caso contrario, se guarda la referencia al parámetro
-en el atributo salvoconducto y se devuelve true.
-• void perderSalvoconducto (): se indica al salvoconducto que ha sido usado (método
-usada()) y se hace nulo este atributo.
-• Boolean tieneSalvoconducto (): indica si la referencia al salvoconducto no es nula.
-• Boolean puedeComprarCasilla (): si el jugador está encarcelado fija el atributo
-puedeComprar a false y a true en caso contrario. Se devuelve el valor de este atributo.
-• Boolean paga (float cantidad): llama al método modificarSaldo con el valor del parámetro
-multiplicado por -1 y devuelve el valor devuelto por modificarSaldo.
-• Boolean pagaImpuesto (float cantidad): si el jugador está encarcelado devuelve false y no
-se realiza ninguna otra acción. En caso contrario se llama al método paga con el mismo
-parámetro y se devuelve lo que devuelve este último método.
-• Boolean pagaAlquiler (float cantidad): el cuerpo de este método tiene el mismo código
-(por ahora) que pagaImpuesto.
-• Boolean recibe (float cantidad): si el jugador está encarcelado devuelve false y no se
-realiza ninguna otra acción. En caso contrario se llama al método modificarSaldo con el
-mismo parámetro y se devuelve lo que devuelve este último método.
-• Boolean modificarSaldo (float cantidad): incrementa el saldo en la cantidad indicada por el
-parámetro e informa al diario. Siempre devuelve true.
-• Boolean moverACasilla (int numCasilla): si el jugador está encarcelado devuelve false y
-no se realiza ninguna otra acción. En caso contrario se fija el atributo numCasillaActual al
-valor del parámetro, el valor de puedeComprar a false, se informa al diario del movimiento
-del jugador y se devuelve true.
-• Boolean puedoGastar (float precio): si el jugador está encarcelado devuelve false y no se
-realiza ninguna otra acción. En caso contrario el método indica si el saldo el mayor o igual
-que el parámetro.
-• Boolean vender (int ip): si el jugador está encarcelado devuelve false y no se realiza
-ninguna otra acción. En caso contrario, si existe la propiedad (método existeLaPropiedad) se
-indica al título de propiedad con número ip que es vendida por el jugador(método vender de
-TituloPropiedad). Si ese proceso de venta se ha realizado satisfactoriamente se elimina la
-propiedad de la lista de propiedades del jugador , se informa al diario de la venta y se
-devuelve true. Si la propiedad no existe o no se puede realizar la venta se devuelve false.
-• Boolean tieneAlgoQueGestionar (): este método indica si el jugador tiene propiedades
-• Boolean puedeSalirCarcelPagando (): este método informa de si el saldo del jugador es
-mayor o igual que el precio que hay que pagar por salir de la cárcel
-• Boolean salirCarcelPagando (): si el jugador está encarcelado y puede salir de la cárcel
-pagando, paga ese precio (método paga), deja de estar encarcelado, se informa al diario de
-este hecho y se devuelve true. En cualquier otro caso devuelve false.
-• Boolean salirCarcelTirando (): se pregunta al dado si el jugador debe salir de la cárcel. En
-caso afirmativo se fija el valor de encarcelado a false y se informa al diario. El valor
-devuelto indica si se ha conseguido salir o no.
-• Boolean pasaPorSalida (): se incrementa el saldo con el método modificarSaldo tanto como
-indique el premio por pasar por la salida. También se informa al diario del evento y siempre
-se devuelve true.
-• int compareTo (Jugador otro): este método delega en el método compare de clase Float
-para comparar el saldo del jugador con el saldo del jugador pasado como parámetro. En
-Ruby se utilizará el operador <=> en vez de el método compareTo y este delegará en el uso
-del mismo operador entre los mismos saldos. Investiga lo que devuelve el método
-compareTo de la interfaz Comparable y el operador <=>*/
     public Boolean debeSerEncarcelado()
     {
         if (encarcelado)
